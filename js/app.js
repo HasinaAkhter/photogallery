41b5acefbba589b8a146f6b0807da50c8a0d2679
Hasina Akhter <hakhter2002@yahoo.com>
2019-02-03 15:03:54 -0500

    Update app.js
    updated copyright info

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $('.para').css('display' , 'none').fadeIn(1600);
 $('img').css('display' , 'none').fadeIn(1600);
 //update copyright info
-const copy = document.querySelector('footer p');
-copy.innerHTML = '&copy; www.phtogallery.hasinaakhter.com 2017-2018 All rights reserved.';
+//const copy = document.querySelector('footer p');
+//copy.innerHTML = '&copy; www.phtogallery.hasinaakhter.com 2017-2018 All rights reserved.';
+//update copyright info dynamically
+const date = new Date().getFullYear();
+document.getElementById("year").innerHTML = date;
